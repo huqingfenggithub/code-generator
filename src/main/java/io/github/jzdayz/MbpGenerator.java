025d1dcebfaa4b03aa5b08fbad289bbb849c9fe2
huqingfeng <huqingfenggithub@163.com>
2023-12-05 13:02:45 +0800

    xml

--- src/main/java/io/github/jzdayz/MbpGenerator.java
+++ src/main/java/io/github/jzdayz/MbpGenerator.java
@@ -61,7 +61,7 @@ public class MbpGenerator {
                 .packageConfig((scanner, builder) ->
                         builder
                                 .parent(codeGeneratorEntity.getMbpPackage())
-                                .pathInfo(Collections.singletonMap(OutputFile.xml, path + File.separator + codeGeneratorEntity.getMbpPackage().replace(".", "/")))
+                                .pathInfo(Collections.singletonMap(OutputFile.xml, path + File.separator + codeGeneratorEntity.getMbpPackage().replace(".", "/") + File.separator + "xml"))
                 )
                 .dataSourceConfig(builder -> {
                     if (StrUtil.isNotBlank(codeGeneratorEntity.getSchema())) {
